86aa7e43c7486c2a4617da4097658bb1356c4da2
YassineBolard <yassine.bolard@etu.univ-tours.fr>
2025-11-14 09:38:07 +0100

    feat(case3): implement feature and fix existing issue

---
+++ src/test/java/fr/polytech/gl/tp3/Case3ShippingCostServiceTest.java
@@ -0,0 +1,23 @@
+package fr.polytech.gl.tp3;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.Test;
+
+public class Case3ShippingCostServiceTest {
+
+    @Test
+    void testCalculateShippingCostIllegalArgument() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Case3ShippingCostService service = new Case3ShippingCostService();
+            service.computeShippingCost(0, false);
+        });
+    }
+
+    @Test
+    void testCalculateShippingCost() {
+        Case3ShippingCostService service = new Case3ShippingCostService();
+        double cost1 = service.computeShippingCost(2.5, false);
+        assertEquals(8.0, cost1);
+    }
+}
