8e31ee522b84ff3426aa1b219ccf556c0c372aea
Guilhem Fauré <pro@guilhemfau.re>
2025-11-21 12:06:24 +0100

    fix: throw error if illegal args

--- src/main/java/fr/polytech/gl/tp3/Case11TaxService.java
+++ src/main/java/fr/polytech/gl/tp3/Case11TaxService.java
@@ -1,28 +1,10 @@
 package fr.polytech.gl.tp3;
 
-/*
- * Case 11 : calcul de TVA.
- * Tâches :
- * - Corriger l'issue de paramètre inutile dans existingVatLabel().
- * - Implémenter computeVat(amount, rate) :
- *      - amount >= 0, rate >= 0
- *      - retourne le montant de TVA
- * - Écrire au moins un test unitaire sur computeVat.
- */
 public class Case11TaxService {
-  double tva = 0;
-
-  // FEATURE : à implémenter
   public double computeVat(double amount, double rate) {
     if (amount < 0 || rate < 0)
-    {
-      return -1;
-    }
-    else
-    {
-      tva = amount * rate;
-      return tva;
-    }
-    // TODO: implémenter en gérant les entrées invalides
+      throw new IllegalArgumentException("Un prix ou taux négatif n’a absolument aucun début de commencement de sens");
+
+    return amount * rate;
   }
 }
